0ad2ca6e6d4e3e0bf863a32eed1a1a850d10dff8
Rina Volovich <rinav@Rinas-MacBook-Air.local>
2021-10-22 17:40:09 +0300

    serve static files attempt 3

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ const app = exp();
 
 app.use(cors());
 
-app.use(exp.static(__dirname+"/public"));
+app.use(exp.static(__dirname+'/public'));
 
-app.get("/", (req, res)  => {
-  res.sendFile(__dirname+ "/index.html");
+app.get('/', (req, res)  => {
+  res.sendFile(__dirname+ '/index.html');
   res.end;
 });
 
 //go to modules - use axios to get the companies 
 //and send back to the frontend
 app.get('/companies', (req, res) => {
+	res.sendFile(__dirname + '/companies.html');
 	companies.companies(req.query.profile)
 		.then(data => {
 			res.send(data);
@@ -26,6 +27,7 @@ app.get('/companies', (req, res) => {
 })
 
 app.get('/people', (req, res) => {
+	res.sendFile(__dirname + '/person.html');
 	person.person(req.query.profile)
 		.then(data => {
 			res.send(data);
